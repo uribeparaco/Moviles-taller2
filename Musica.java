bb398713440ab3d9f0e1bdaf5f41bcc3ddbba9d0
uribeparaco <juan.baezb@uniagustiniana.edu.co>
2023-08-26 20:53:50 -0500

    se agrego proyecto

--- Musica.java
+++ Musica.java
@@ -1,52 +1,63 @@
 import javax.sound.sampled.*;
 import java.io.File;
 import java.util.Scanner;
-
+//////////////////////////////////////////////////////////////
 public class Musica {
     public static void main(String[] args) {
-        Thread musicThread = new Thread(() -> {
+        Thread musica = new Thread(() -> {
             try {
-                File audioFile = new File("BLESSD-RYAN-CASTRO-OJITOS-ROJOS-REMIX-_VIAJE-3_.wav"); 
-                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioStream);
-                clip.start();
-                Thread.sleep(clip.getMicrosecondLength() / 40);
+                File audio = new File("BLESSD-RYAN-CASTRO-OJITOS-ROJOS-REMIX-_VIAJE-3_.wav"); 
+                AudioInputStream audio01 = AudioSystem.getAudioInputStream(audio);
+                Clip reproduccion = AudioSystem.getClip();
+                reproduccion.open(audio01);
+                reproduccion.start();
+                Thread.sleep(reproduccion.getMicrosecondLength() / 40);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
-
+////////////////////////////////////////////////////////////////////
         Thread inputThread = new Thread(() -> {
             try (Scanner scanner = new Scanner(System.in)) {
-                System.out.print("Ingresa tu nombre: ");
+                System.out.println("INGRESE SU NOMBRE");
                 String nombre = scanner.nextLine();
-                System.out.print("Ingresa tu edad: ");
+                System.out.println("INGRESE SU EDAD: ");
                 int edad = Integer.parseInt(scanner.nextLine());
-                System.out.print("¿Cómo te sientes hoy? ");
+                System.out.println("COMO TE SIENTES BB");;
                 String estado = scanner.nextLine();
+                System.out.println("CARRERA: ");
+                String carrera = scanner.nextLine();
+                System.out.println("SEMESTRE: ");
+                String semestre = scanner.nextLine();
 
-                System.out.println("\nGracias por proporcionar tus datos.");
-                System.out.println("Nombre: " + nombre);
-                System.out.println("Edad: " + edad);
-                System.out.println("Estado de ánimo: " + estado);
-            } catch (NumberFormatException e) {
+
+                System.out.println("\n GRACIAS POR INGRESAR LOS DATOS JOVEN MUY AMABLE ES USTED.");
+                System.out.println("NOMBRE: " + nombre);
+                System.out.println("EDAD: " + edad);
+                System.out.println("ESTADO DE ANIMO: " + estado);
+                System.out.println("CARRERA: " +carrera);
+                System.out.println("SEMESTRE: " +semestre);
+            } catch (NumberFormatException e) 
+            
+            {
                 System.out.println("Ingresa una edad válida.");
             }
         });
 
-        System.out.println("Iniciando reproducción de música y solicitud de datos...");
+        
 
-        musicThread.start();
+        musica.start();
         inputThread.start();
 
         try {
-            musicThread.join();
+            musica.join();
             inputThread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        System.out.println("Programa finalizado. ¡Sigue disfrutando la canción!");
     }
 }
+
+
+
+
